905a31989bd28b6fce9ba84e1921f05d9890c0c6
Sim Gill <simgill89@gmail.com>
2017-06-29 10:26:49 -0700

    moving on to projections

--- client/src/components/usershomepics.js
+++ client/src/components/usershomepics.js
@@ -20,8 +20,34 @@ class UserHomePics extends React.Component {
   }
    
  
-   componentDidMount() { 
-   }
+    componentDidMount() {
+    $(function(){
+      cycle();  
+      $('body :not(.nav), i:not(.ion-navicon)').click(function(){
+      });
+
+      function cycle(){
+        var x = 0;
+        setInterval(language, 3000);
+        function language(){
+          $('.hello').fadeOut(1000, function(){
+            $(this).text(hello[x + 1]);
+            $(this).fadeIn(1000);
+            x++;        
+            if (x > hello.length - 2) {
+              x = -1;
+            }
+          }
+        )};
+      }
+      var hello = [
+        '"Lorem ipsum dolor sit amet, luctus amet rutrum, commodo, ac in tellus sit elit leo. Integer amet maecenas semper pretium.Lorem ipsum dolor sit amet, luctus amet rutrum, commodo, ac in tellus sit elit leo. Integer amet maecenas semper pretium"',
+        '"Inceptos a, malesuada potenti lacus et dolores. Aliquam in tristique pellentesque expedita dolor urna"',
+        
+        
+      ]
+    });
+  }
 
 
     render() {
@@ -32,13 +58,27 @@ class UserHomePics extends React.Component {
            <h1 className="tsttext projectsummary"> <span className="userslaxh" > / </span> Current Status </h1>
           </div>
           <div className="userbottompics">
+            <div className="bottominner">
+              <div className="userleftpics">
+                <div className="uleftTopRow">
+                  <div className="topleftsleftdiv">
+                  </div>
+                  <div className="topleftsrightdiv">
+                  </div>
+                </div>
+                <div className="uleftbottompRow">
+                  <div className="bottomrowleftdiv">
+                  </div>
+                  <div className="bottomrowrightdiv">
+                  </div>
+                </div>
+              </div>
 
-            <div className="userleftpics">
+              <div className="userrightpics">
+              <h3 className="liveupdatestext"> Live Updates </h3>
+              <p className="hello"> </p>
+              </div>
             </div>
-
-            <div className="userrightpics">
-            </div>
-
           </div>
 
 
