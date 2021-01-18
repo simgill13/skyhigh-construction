40f0c512f3fad786b8a7ca16f17854730a973973
Simran Gill <simgill89@gmail.com>
2021-01-18 13:45:31 -0800

    disable mlab

--- server/index.js
+++ server/index.js
@@ -205,15 +205,18 @@ function runServer(port=3001) {
     return new Promise((resolve, reject) => {
 
 
-         mongoose.connect(DATABASE_URL, err => {
-            if(err) {
-              return reject(err);
-        }
-            console.log('Db connected');
-            server = app.listen(port, () => {
-                resolve();
-            }).on('error', reject);
-        });
+        //  mongoose.connect(DATABASE_URL, err => {
+        //     if(err) {
+        //       return reject(err);
+        // }
+        //     console.log('Db connected');
+        //     server = app.listen(port, () => {
+        //         resolve();
+        //     }).on('error', reject);
+        // });
+        server = app.listen(port, () => {
+          resolve();
+      }).on('error', reject);
     });
 }
 
